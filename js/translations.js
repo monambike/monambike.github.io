42dd53e21b1641d988cad1c0b5ff98da6df9d178
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-07-23 02:04:04 -0300

    #6 Translation Sistem Done

---
+++ js/translations.js
@@ -0,0 +1,11 @@
+var translate_json = `{
+	"en-US":{
+		"developer": "Developer",
+		"welcome": "Welcome to my site!"
+	},
+
+	"pt-BR":{
+		"developer": "Desenvolvedor",
+		"welcome": "Seja bem vindo!"
+	}
+}`
