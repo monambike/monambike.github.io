d3c899cfd4cc59e9392b95bbcdbceaf29338f753
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-07-25 04:05:51 -0300

    Moved functions to translationArchive.js

--- js/functions.js
+++ js/functions.js
@@ -14,28 +14,7 @@ document.body.onload = function(event){
 	// Contador
 	var i = 0;
 
-	// Verifica se o idioma do browser é compatível com o do site
-	for(i = 0; i < supportedLang.length; i++){
-		/* Se for compatível, e se o arquivo JSON foi carregado
-		dá o valor para a variável e traduz */
-		if(browserLang === supportedLang[i] && translationArquiveJson !== 0){
-			pageLang = supportedLang[i];
-			localStorage.setItem("language", pageLang);
-			translatePage();
-			return;
-		}
-	}
-}
-
-function translatePage(pageLang){
-	var translateElements = document.getElementsByClassName("translate");
-
-	if(pageLang !== 0){
-		for(i = 0; i < translateElements.length; i++){
-			translateElements[i].innerHTML = translationArquiveJs[pageLang][translateElements[i].getAttribute("langSrc")];
-		}
-		return;
-	}
+	translateBrowserLanguage();
 }
 
 function switchLanguage(){
