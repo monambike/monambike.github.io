3e69f342d82e6493c8209f13495cb46e9bc4f370
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-09-09 18:53:50 -0300

    Add Javascript function requested on issue #33

--- js/functionsPage.js
+++ js/functionsPage.js
@@ -1,11 +1,19 @@
 // VARIABLES
 var divDisplay = document.getElementById("div-display");
+var svgDots = document.getElementsByClassName("svg-dot");
+var i = 0;
 
 // Display content
 divDisplay.style.display = "block";
 updateIframeSize();
 
-// Function taao select dot
+// Function to select dot
 function selectDot(dotSelected){
-	
+	console.log("You've selected dot number " + dotSelected + " that corresponds to " + svgDots[dotSelected] + ".");
+
+	for(i = 0; i < svgDots.length; i++){
+		svgDots[i].style.color = "var(--gray-2)";
+	}
+
+	svgDots[dotSelected].style.color = "var(--white)"
 }
