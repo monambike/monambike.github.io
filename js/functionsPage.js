a905493e8cdc7114c7744c60a07f0b91a2eae40f
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-09-09 19:36:41 -0300

    Apply final changes of gallery

--- js/functionsPage.js
+++ js/functionsPage.js
@@ -2,6 +2,7 @@
 var divDisplay = document.getElementById("div-display");
 var svgDots = document.getElementsByClassName("svg-dot");
 var galleries = document.getElementsByClassName("div-gallery");
+var slideTitle = document.getElementsByClassName("h3-slide-title");
 var i = 0;
 
 // Display content
@@ -23,13 +24,16 @@ function selectDot(dotSelected){
 
 function updateSlide(dotSelected){
 	for(i = 0; i < galleries.length; i++){
+		slideTitle[i].classList.add("hidden");
 		galleries[i].classList.add("hidden");
 	}
 
 	try{
+		slideTitle[dotSelected].classList.remove("hidden");
 		galleries[dotSelected].classList.remove("hidden");
 	}
 	catch(e){
-		console.log("It was not possible to find a gallery");
+		console.log("It was not possible to find a gallery"
+			+ "\n" + e);
 	}
 }
