7b42dd8d88c7be1d31c057a65b4240614d4a0ec7
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-09-09 18:39:08 -0300

    Add two comments

--- js/functionsPage.js
+++ js/functionsPage.js
@@ -1,3 +1,10 @@
+// VARIABLES
 var divDisplay = document.getElementById("div-display");
+
+// Display content
 divDisplay.style.display = "block";
 updateIframeSize();
+
+function selectDot(dotSelected){
+	
+}
